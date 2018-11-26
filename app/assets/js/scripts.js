27c2756f58bfa5af213195f8f2c41e760fe0a7fd
AndrZakhara <art.pruttov@gmail.com>
2018-11-27 01:09:42 +0200

    first commit

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * website
+ * Fiercely quick and opinionated front-ends
+ * 
+ * @author Andr Zakhar
+ * @version 1.0.0
+ * Copyright 2018. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // FastShell
+  });
+
+})(jQuery, window, document);
