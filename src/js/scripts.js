1d3b619fbd6ccd8e070fb05fdc357660249700ac
unknown <art.pruttov@gmail.com>
2018-12-11 14:27:09 +0200

    add function createElement

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,19 +1,67 @@
-function createNewElement(element, selector) {
+let inputValue = '';
+let selectedElement = '';
 
-  let el = document.createElement(element);
-  el.classList.add(selector);
-  // document.body.appendChild(el);
-  document.body.insertAdjacentElement('afterBegin', el);
-  console.log(el);
+function createAppStructure() {
+  const inputSearch = createElement('input', { className: 'search-input', type: 'text', placeholder: 'Selector' });
+  const btnSearch = createElement('input', { className: 'search-btn', type: 'button', value: 'search' });
+   
+  const btnPrev = createElement('input', { className: 'prev-btn', type: 'button', value: 'Prev' });
+  const btnNext = createElement('input', { className: 'next-btn', type: 'button', value: 'Next' });
+  const btnParent = createElement('input', { className: 'parent-btn', type: 'button', value: 'Parent' });
+  const btnChildren = createElement('input', { className: 'children-btn', type: 'button', value: 'Children' });
+  
+  const textFieldWrapper = createElement('div', { className: 'text-field-wrapper' }, inputSearch, btnSearch);
+  const btnWrapper = createElement('div', { className: 'btn-wrapper' }, btnPrev, btnNext, btnParent, btnChildren);
+  
+  const mainAppWrapper = createElement('div', { className: 'selector-search-app' }, textFieldWrapper, btnWrapper);
+
+  btnSearch.addEventListener('click', onclickSearch);
+
+  document.body.insertAdjacentElement('afterBegin', mainAppWrapper);
+}
+
+const onclickSearch = (e) => {
+  inputValue = document.querySelector('.search-input').value;
+  
+  document.querySelector('.search-input').value = '';
+  console.log(inputValue);
+  addClass();
+}
+
+const addClass = () => {
+  console.log('add class');
+  console.log(selectedElement);
+  
+  if(selectedElement === '') {
+    const selected = document.querySelector(inputValue);
+    
+    if (selected !== undefined && selected !== null) {
+      selectedElement = selected;
+      selectedElement.classList.add('selected-element-search');
+    }
+    console.log(selectedElement); 
+    
+  } 
+  else if(selectedElement !== null || selectedElement !== undefined) {
+    selectedElement.classList.remove('selected-element-search');
+    const selected = document.querySelector(inputValue);
+    
+    if (selected !== undefined && selected !== null) {
+      selectedElement = selected;
+      selectedElement.classList.add('selected-element-search');
+    }
+  }
+ 
+}
+const removeClass = () => {
+  console.log('remove class');
 }
 
 function appInit() {
-  console.log('app init');
-  createNewElement('div', 'selector-search-app');
+  createAppStructure();
 }
 
 function createElement(tag, props, ...children) {
-
   const element = document.createElement(tag);
 
   Object.keys(props).forEach(key => (element[key] = props[key]));
@@ -30,15 +78,4 @@ function createElement(tag, props, ...children) {
   return element;
 }
 
-const span = createElement(
-  "span", { className: "result-value" },
-  'Hi!!!'
-);
-const paragraph = createElement(
-  "p", { className: "temperature-value" },
-  "Вы ввели: ",
-  span
-);
-
-console.log(paragraph);
 document.addEventListener("DOMContentLoaded", appInit);
