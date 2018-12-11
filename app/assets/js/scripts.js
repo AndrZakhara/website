e122156ceecf91ff97f38eee13876c6cf8b8f364
AndrZakhara <art.pruttov@gmail.com>
2018-12-11 22:02:26 +0200

    add spaces, remove console.logs

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -8,27 +8,27 @@
  */
 let inputValue = '',
     selectedElement = '',
-    previous = false,
-    next = false,
-    parent = false,
+    // previous = false,
+    // next = false,
+    // parent = false,
     parentNode = '',
-    children = false,
+    // children = false,
     childrenNode = '';
 
 function createAppStructure() {
   const inputSearch = createElement('input', { className: 'search-input', type: 'text', placeholder: 'Selector' });
   const btnSearch = createElement('input', { className: 'search-btn', type: 'button', value: 'search' });
-   
+
   const btnPrev = createElement('input', { className: 'prev-btn', type: 'button', value: 'Prev' });
   const btnNext = createElement('input', { className: 'next-btn', type: 'button', value: 'Next' });
   const btnParent = createElement('input', { className: 'parent-btn', type: 'button', value: 'Parent' });
   const btnChildren = createElement('input', { className: 'children-btn', type: 'button', value: 'Children' });
-  
+
   const textFieldWrapper = createElement('div', { className: 'text-field-wrapper' }, inputSearch, btnSearch);
   const btnWrapper = createElement('div', { className: 'btn-wrapper' }, btnPrev, btnNext, btnParent, btnChildren);
-  
+
   const mainAppWrapper = createElement('div', { className: 'selector-search-app' }, textFieldWrapper, btnWrapper);
-  
+
   btnSearch.disabled = true;
   btnPrev.disabled = true;
   btnNext.disabled = true;
@@ -39,50 +39,47 @@ function createAppStructure() {
   inputSearch.addEventListener('input', handleInput);
   btnParent.addEventListener('click', handleParent);
 
-  document.body.insertAdjacentElement('afterBegin', mainAppWrapper);
+  document.body.insertAdjacentElement('beforeend', mainAppWrapper);
 }
 
-const handleSearch = (e) => {
+const handleSearch = () => {
   inputValue = document.querySelector('.search-input').value;
-  
   document.querySelector('.search-input').value = '';
-  console.log(inputValue);
+  document.querySelector('.search-btn').disabled = true;
   addClass();
-}
-const handleInput = (e) => {  
-  if(e.target.value === '') {
-    document.querySelector('.search-btn').disabled = true;
-  } 
-  else {
-    document.querySelector('.search-btn').disabled = false;
-  };
-}
+};
+
+const handleInput = (e) => {
+  (e.target.value === '')
+    ? document.querySelector('.search-btn').disabled = true
+    :document.querySelector('.search-btn').disabled = false
+};
+
 const handleParent = () => {
   selectedElement.classList.remove('selected-element-search');
   selectedElement = parentNode;
   selectedElement.classList.add('selected-element-search');
   findRelative(selectedElement);
-}
+};
 
 const addClass = () => {
   console.log('add class');
   console.log(selectedElement);
-  
+
   if(selectedElement === '') {
     const selected = document.querySelector(inputValue);
-    
+
     if (selected !== undefined && selected !== null) {
       selectedElement = selected;
       selectedElement.classList.add('selected-element-search');
       findRelative(selectedElement);
     }
-    console.log(selectedElement); 
-    
-  } 
+    // console.log(selectedElement);
+  }
   else if(selectedElement !== null || selectedElement !== undefined) {
     selectedElement.classList.remove('selected-element-search');
     const selected = document.querySelector(inputValue);
-    
+
     if (selected !== undefined && selected !== null) {
       selectedElement = selected;
       selectedElement.classList.add('selected-element-search');
@@ -91,23 +88,27 @@ const addClass = () => {
   }
 
   document.querySelector('.search-btn').disabled = true;
-  scrollToElement('.selected-element-search'); 
-}
-const removeClass = () => {
-  if(selectedElement !== null || selectedElement !== undefined) {
-    selectedElement.classList.remove('selected-element-search');
-  }
-}
+  scrollToElement('.selected-element-search');
+};
+
+// const removeClass = () => {
+//   if(selectedElement !== null || selectedElement !== undefined) {
+//     selectedElement.classList.remove('selected-element-search');
+//   }
+// };
 
 function scrollToElement(theElement) {
-    if (typeof theElement === "string") theElement = document.querySelector(theElement);
-    var selectedPosX = 0;
-    var selectedPosY = 0;
-    while (theElement != null) {
+  let selectedPosX = 0;
+  let selectedPosY = 0;
+
+  if (typeof theElement === "string") theElement = document.querySelector(theElement);
+
+    while (theElement !== null) {
         selectedPosX += theElement.offsetLeft;
         selectedPosY += theElement.offsetTop;
         theElement = theElement.offsetParent;
     }
+
     selectedPosY = selectedPosY-20;
     window.scrollTo(selectedPosX, selectedPosY);
 }
@@ -123,7 +124,7 @@ function findRelative(element) {
   console.log(`tag: ${tagNameParent}`);
   console.log(`tagChild: ${child}`);
 
-  if(element.parentNode !== undefined 
+  if(element.parentNode !== undefined
     && tagNameParent !== 'BODY'
     && tagNameParent !== 'HTML'
     && tagNameParent !== 'document'
@@ -132,14 +133,17 @@ function findRelative(element) {
     console.log(element.parentNode);
     document.querySelector('.parent-btn').disabled = false;
     parentNode = element.parentNode;
-  } else {
+  }
+  else {
     document.querySelector('.parent-btn').disabled = true;
     parentNode = '';
   }
-  if(child !== undefined) {    
+
+  if(child !== undefined) {
     document.querySelector('.children-btn').disabled = false;
     childrenNode = child.tagName;
-  } else {    
+  }
+  else {
     document.querySelector('.children-btn').disabled = true;
     childrenNode = '';
   }
